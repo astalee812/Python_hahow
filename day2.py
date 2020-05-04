49baa55e36f2ef698b4add511c8a1c722b5ffa14
Asta Lee <astalee12@gmail.com>
2020-05-05 00:50:41 +0800

    Update day2.py

--- day2.py
+++ day2.py
@@ -24,3 +24,53 @@ a.append(5) # 加入數字5到a陣列中
 a
 a.sort() # 排序
 a
+
+# 雙重陣列
+c = ['123',['456','789']]
+c[1]
+c[1][1]
+
+
+# 等於也算是一個陣列
+d = '你好嗎'
+d[0]
+
+
+# Dictionary : 都是用中括號包起來，1個key對應一個value
+# 可以新增無限多筆資料! 類似json
+dic = {'key1':'Value1',
+       'key2':'Value2',
+       'key3':'Value3',
+       'getme': 'Yooooooooooo~'}
+
+# 取出Dictionary資料
+dic['key2']
+
+# 問題
+# 嘗試取出 'Yooooooooooo~'
+dic['getme']
+
+
+# dictionary裡面可以再包一個dictionary
+# key可以使用數字
+{'key1':'Value1',
+   'key2':'Value2',
+   'key3':'Value3',
+   'key4': {'key1':'Value1',
+           'key2':'Value2',
+           'key3':'Value3',
+           'key4': 'Value4'}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
