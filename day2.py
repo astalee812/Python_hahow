4ae1406c4f54ad8c0def86ee287e6dfd3c0e0027
Asta Lee <astalee12@gmail.com>
2020-05-05 00:30:59 +0800

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 00:23:17 2020
+
+@author: Asta Lee
+"""
+
+# 陣列
+a = [1,2,3,4,5]
+
+# 要切記! 編號是從0開始編號
+a[0]
+
+# 取出List資料
+a[4]
+
+# 計算長度
+print(len(a))
+
+# 常見操做
+a.append(7) # 加入數字7到a陣列中
+a.append(9)
+a.append(5) # 加入數字5到a陣列中
+a
+a.sort() # 排序
+a
